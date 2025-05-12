ea5b0fdba7424873abaf4a86e0f964f0b3d12992
Yashika <yvijayvargiyadev@gmail.com>
2025-05-12 11:47:01 +0530

    Added spec for single number

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -6,5 +6,10 @@ RSpec.describe StringCalculator do
       calc = StringCalculator.new
       expect(calc.add("")).to eq(0)
     end
+
+    it 'returns the number itself when a single number is given' do
+      calc = StringCalculator.new
+      expect(calc.add("1")).to eq(1)
+    end
   end
 end
